103eadfb4ae0cc76d46318859b1f69c02eb2ec94
Malte Wirz <mail@maltewirz.com>
2020-03-18 13:53:47 +0100

    Finsished cloud storage function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,5 @@ export { basicHTTP2, api } from './http';
 export { createUserRecord } from './auth';
 
 export { gameCount, userTrend } from './firestore';
+
+export { resizeAvatar } from './storage';
--- functions/src/storage.ts
+++ functions/src/storage.ts
@@ -1,20 +1,16 @@
 import * as functions from 'firebase-functions';
-// import * as admin from 'firebase-admin';
-
 import * as storage from '@google-cloud/storage'
 const gcs = new storage.Storage();
 
-// import * as fs from 'fs-extra';
-
 import { tmpdir } from 'os';
 import { join } from 'path';
-
 import * as sharp from 'sharp';
-
 
 export const resizeAvatar = functions.storage
     .object()
     .onFinalize( async object => {
+        console.log('running resizeAvatar');
+        
         const bucket = gcs.bucket(object.bucket);
 
         const filePath = object.name;
@@ -25,7 +21,7 @@ export const resizeAvatar = functions.storage
             const avatarFileName = 'avatar_' + fileName;
             const tmpAvatarPath = join(tmpdir(), avatarFileName);
     
-            if (fileName?.includes('avatar_')) {
+            if (fileName?.includes('avatar_')) { // this part ensures no infinite loop
                 console.log('exiting function');
                 return false;
             }
@@ -44,5 +40,4 @@ export const resizeAvatar = functions.storage
         } else {
             return;
         }
-      
     })
